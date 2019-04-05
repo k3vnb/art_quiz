880d1795672bbb84bccdf5fb4b1832e3d88434cb
Kevin Boyle <papershack@gmail.com>
2019-04-04 23:36:59 -0700

    begin js functionality

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 'use strict'
 
+function startQuiz(){
+    // change view from beginning page to first question
+    $('.start-quiz').on('click', () => {
+        console.log('quiz started');
+        $('.start-page').hide();
+        $('.question-page-one').show();
+    })
+}
+
+function handleStartQuiz(){
+    startQuiz();
+}
+
+$(handleStartQuiz)
